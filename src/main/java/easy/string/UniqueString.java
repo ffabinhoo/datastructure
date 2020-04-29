950c2cbc09a5bd2daa01875bbdf916a27143108c
fabiocosta <fabiocosta@192.168.0.104>
2020-04-29 12:57:04 +1000

    Unique Characters and Microsoft Question

---
+++ src/main/java/easy/string/UniqueString.java
@@ -0,0 +1,23 @@
+package easy.string;
+
+public class UniqueString {
+
+	public static void main(String[] args) {
+		UniqueString unique = new UniqueString();
+		
+		String s = "fabio";
+		boolean result = unique.isUnique(s );
+		System.out.println(result);
+	}
+	
+	
+	private boolean isUnique(String s) {
+		int[] cnt = new int[26];
+		for (char c : s.toCharArray()) {
+			if (++cnt[c - 'a'] > 1)
+				return false;
+		}
+		return true;
+	}
+
+}
