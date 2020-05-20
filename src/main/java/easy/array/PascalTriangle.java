460a14396cc69caed88375fd5dec5f713853a50b
Fabio Costa <fabiocosta@Fabios-MacBook-Pro.local>
2020-05-20 14:45:27 +1000

    pascal triangle

--- src/main/java/easy/array/PascalTriangle.java
+++ src/main/java/easy/array/PascalTriangle.java
@@ -3,13 +3,27 @@ package easy.array;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ * Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ */
 public class PascalTriangle {
 
 	public static void main(String[] args) {
 		System.out.println(generate(5));
 
 	}
-
+	
 	public static List<List<Integer>> generate(int numRows) {
 		List<List<Integer>> triangle = new ArrayList<List<Integer>>();
 		
@@ -23,7 +37,6 @@ public class PascalTriangle {
 		for (int i = 1; i < numRows; i++) {
 
 			List<Integer> prev_row = triangle.get(i - 1);
-
 			List<Integer> curr_row = new ArrayList<Integer>();
 
 			curr_row.add(1);
@@ -35,9 +48,6 @@ public class PascalTriangle {
 			triangle.add(curr_row);
 
 		}
-
 		return triangle;
-
 	}
-
 }
