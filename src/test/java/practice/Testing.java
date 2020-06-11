b77fc5f0edd0dc5495584560632aacacf6600300
Fabio Costa <ffabinhoo@gmail.com>
2020-06-11 21:26:28 +1000

    Optional more tests

--- src/test/java/practice/Testing.java
+++ src/test/java/practice/Testing.java
@@ -23,8 +23,37 @@ public class Testing {
 	
 	@Test
 	public void whenCreatesOptional_thenCorrect() {
-	    Optional<String> str = Optional.of("String");
+	    Optional<String> str = Optional.of("testing");
 	    assertTrue(str.isPresent());
 	}
+	
+	@Test
+	public void givenOptional_whenIfPresentWorks_thenCorrect() {
+	    Optional<String> opt = Optional.of("testing");
+	    opt.ifPresent(name -> System.out.println(name.length()));
+	}
+	
+	@Test
+	public void whenOrElseWorks_thenCorrect() {
+	    String nullName = null;
+	    String name = Optional.ofNullable(nullName).orElse("testing");
+	    assertEquals("testing", name);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void whenOrElseThrowWorks_thenCorrect() {
+	    String nullName = null;
+	    Optional.ofNullable(nullName).orElseThrow(
+	      IllegalArgumentException::new);
+	}
+	
+	@Test
+	public void givenOptional_whenGetsValue_thenCorrect() {
+	    Optional<String> opt = Optional.of("testing");
+	    String name = opt.get();
+	    assertEquals("testing", name);
+	}
+	
+	
 
 }
