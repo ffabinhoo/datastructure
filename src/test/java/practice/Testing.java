8185e69022147d38eb2aecd32f25fa49329300fa
Fabio Costa <ffabinhoo@gmail.com>
2020-06-11 21:15:24 +1000

    Optional tests

--- src/test/java/practice/Testing.java
+++ src/test/java/practice/Testing.java
@@ -1,5 +1,10 @@
 package practice;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
 import org.junit.Test;
 
 public class Testing {
@@ -7,9 +12,19 @@ public class Testing {
 	
 	@Test
 	public void test() {
-		
-		
-		System.out.println(3/2);
+		assertEquals(0, 2/3);
+	}
+	
+	@Test
+	public void whenCreatesEmptyOptional_thenCorrect() {
+	    Optional<String> empty = Optional.empty();
+	    assertFalse(empty.isPresent());
+	}
+	
+	@Test
+	public void whenCreatesOptional_thenCorrect() {
+	    Optional<String> str = Optional.of("String");
+	    assertTrue(str.isPresent());
 	}
 
 }
